48d01b9209960b9b53168a130a1a86cf13f4cd49
Micael Fischmann <micael.fischmann@agst.com.br>
2019-10-21 15:31:48 -0300

    :sparkles: Adiciona metodo checkExp

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 
 public class App {
     public static void main(String[] args) {
-        LinkedQueue queue = new LinkedQueue();
+        /*LinkedQueue queue = new LinkedQueue();
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
@@ -16,6 +16,68 @@ public class App {
         stack.push(3);
         while (!stack.isEmpty()) {
             System.out.println(stack.pop());
-        }        
+        }    */
+        
+        System.out.println(checkExp("()"));
+        System.out.println(checkExp("())"));
+        System.out.println(checkExp("()]]"));
+        System.out.println(checkExp("(){[()]}[()()]"));
+        System.out.println(checkExp("((()"));
+        System.out.println(checkExp("(){[(]]}[()()]"));
+    }
+
+    public static boolean checkExp(String s){
+        if(s.length()%2==1){
+            return false;
+        }
+        
+        ArrayStack pilha = new ArrayStack();
+
+        for(int i = 0; i < s.length(); i++){
+            char x = s.charAt(i);
+            switch(x){
+                case '{':
+                pilha.push(1);
+                if(pilha.size()>s.length()-i){
+                    System.out.println("Excesso de aberturas");
+                    return false;
+                }
+                break;
+                case '(':
+                pilha.push(2);
+                if(pilha.size()>s.length()-i){
+                    System.out.println("Excesso de aberturas");
+                    return false;
+                }
+                break;
+                case '[':
+                pilha.push(3);
+                if(pilha.size()>s.length()-i){
+                    System.out.println("Excesso de aberturas");
+                    return false;
+                }
+                break;                
+                case '}':
+                if(pilha.isEmpty()||pilha.pop() != 1){
+                    return false;
+                }
+                break;
+                case ')':
+                if(pilha.isEmpty()||pilha.pop() != 2){
+                    return false;
+                }
+                break;
+                case ']':
+                if(pilha.isEmpty()||pilha.pop() != 3){
+                    return false;
+                }
+                break;
+                default:
+                System.out.println("Simbolo nao suportado");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
